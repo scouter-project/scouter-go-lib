61e08ae41516a9be5194d1f3d393c00060c24718
iwindfree <iwindfree@outlook.com>
2021-11-11 18:05:53 +0900

    add aws serverless pack

--- common/constants/packconstant/packconstants.go
+++ common/constants/packconstant/packconstants.go
@@ -15,9 +15,11 @@ const (
 	K8S_CLUSTER__INFO byte = 66
 	ALERT             byte = 70
 	OBJECT            byte = 80
-	CLOUD_WATCH		  byte = 11
-	XRay			  byte = 12
-	CLOUD_OBJECT	  byte = 13
+	CLOUD_WATCH       byte = 11
+	XRay              byte = 12
+	CLOUD_OBJECT      byte = 13
 	CLOUD_ITEMS       byte = 14
 	STACKDRIVER       byte = 15
+	XRAY_HEATMAP      byte = 16
+	AWS_LAMBDA        byte = 17
 )
