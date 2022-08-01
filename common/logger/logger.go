99aa63942393f4ec363e3198edbe60a82540d0cf
iwindfree <iwindfree@outlook.com>
2022-08-01 17:24:49 +0900

    initialize logger with filename

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -9,6 +9,27 @@ import (
 )
 
 //Init initializes logger
+func Init2(logName string) {
+
+	p, err := util.GetAppPath()
+	if err != nil {
+
+	}
+	logPath := filepath.Join(p, "logs")
+	util.MakeDir(logPath)
+	fileName := filepath.Join(logPath, logName)
+	logfile, e := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if e != nil {
+		log.Fatalln("cannot open log file")
+	}
+
+	Trace = log.New(io.MultiWriter(logfile, os.Stdout), "trace:", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(io.MultiWriter(logfile, os.Stdout), "info:", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(io.MultiWriter(logfile, os.Stdout), "warning:", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(logfile, os.Stderr), "error:", log.Ldate|log.Ltime|log.Lshortfile)
+
+}
+
 func Init() {
 
 	p, err := util.GetAppPath()
